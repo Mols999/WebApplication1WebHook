bf67e2c6f1edefabd68329be32557761bc445470
Mols999 <114399978+Mols999@users.noreply.github.com>
2024-05-30 08:58:01 +0200

    Add project files.

---
+++ WebApplication1WebHook/Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Channels;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication1WebHook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            return View();
+        }
+
+        // Method to create a new customer using a SOAP
+        [HttpPost]
+        public async Task<ActionResult> CreateCustomer(string username, string email, string firstName, string lastName)
+        {
+            string serviceUrl = "http://bc-container:7047/BC/WS/CRONUS%20Danmark/Codeunit/CustomerManagement";
+            string soapAction = "urn:microsoft-dynamics-schemas/codeunit/CustomerManagement:CreateCustomer";
+
+            string userName = "ADMIN";
+            string password = "Password";
+
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.TransportCredentialOnly)
+            {
+                Security = { Transport = { ClientCredentialType = HttpClientCredentialType.Basic } },
+                MaxReceivedMessageSize = 65536
+            };
+
+            var endpoint = new EndpointAddress(serviceUrl);
+
+            using (var factory = new ChannelFactory<IWCFService>(binding, endpoint))
+            {
+                factory.Credentials.UserName.UserName = userName;
+                factory.Credentials.UserName.Password = password;
+
+                var client = factory.CreateChannel();
+
+                var customerData = new
+                {
+                    name = $"{firstName} {lastName}".Trim(),
+                    email = email,
+                    username = username
+                };
+
+                var jsonContent = JsonConvert.SerializeObject(customerData);
+
+                using (new OperationContextScope((IContextChannel)client))
+                {
+                    HttpRequestMessageProperty requestMessage = new HttpRequestMessageProperty();
+                    requestMessage.Headers["SOAPAction"] = $"\"{soapAction}\"";
+                    OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = requestMessage;
+
+                    bool result = client.CreateCustomer(jsonContent);
+                    if (result)
+                    {
+                        return Content("CreateCustomer call result: Success");
+                    }
+                    else
+                    {
+                        return Content("CreateCustomer call result: Failed");
+                    }
+                }
+            }
+        }
+
+        // Method to create a new sales order using a SOAP
+        [HttpPost]
+        public async Task<ActionResult> CreateSalesOrder(string customerEmail, string productNumber, decimal quantity)
+        {
+            string serviceUrl = "http://bc-container:7047/BC/WS/CRONUS%20Danmark/Codeunit/SalesOrderManagement";
+            string soapAction = "urn:microsoft-dynamics-schemas/codeunit/SalesOrderManagement:CreateSalesOrder";
+
+            string userName = "ADMIN";
+            string password = "Password";
+
+            var salesOrderData = new
+            {
+                customerEmail = customerEmail,
+                productNumber = productNumber,
+                quantity = quantity
+            };
+
+            var jsonContent = JsonConvert.SerializeObject(salesOrderData);
+
+            string soapEnvelope = $@"
+            <soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:microsoft-dynamics-schemas/codeunit/SalesOrderManagement'>
+               <soapenv:Header/>
+               <soapenv:Body>
+                  <urn:CreateSalesOrder>
+                     <urn:salesOrderJson>{System.Security.SecurityElement.Escape(jsonContent)}</urn:salesOrderJson>
+                  </urn:CreateSalesOrder>
+               </soapenv:Body>
+            </soapenv:Envelope>";
+
+            using (var httpClientHandler = new HttpClientHandler { Credentials = new NetworkCredential(userName, password) })
+            using (var httpClient = new HttpClient(httpClientHandler))
+            {
+                var httpContent = new StringContent(soapEnvelope, Encoding.UTF8, "text/xml");
+                httpContent.Headers.Add("SOAPAction", soapAction);
+
+                var response = await httpClient.PostAsync(serviceUrl, httpContent);
+                string responseString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Content("CreateSalesOrder call result: Success");
+                }
+                else
+                {
+                    return Content("CreateSalesOrder call result: Failed");
+                }
+            }
+        }
+
+        // Interface for the CustomerManagement and SalesOrderManagement SOAP
+        [ServiceContract(Namespace = "urn:microsoft-dynamics-schemas/codeunit/CustomerManagement")]
+        public interface IWCFService
+        {
+            [OperationContract(Action = "urn:microsoft-dynamics-schemas/codeunit/CustomerManagement:CreateCustomer")]
+            bool CreateCustomer(string customerJson);
+
+            [OperationContract(Action = "urn:microsoft-dynamics-schemas/codeunit/SalesOrderManagement:CreateSalesOrder")]
+            bool CreateSalesOrder(string salesOrderJson);
+        }
+    }
+}
---
+++ WebApplication1WebHook/GenericJsonWebHookHandler.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1WebHook.Controllers;
+
+namespace WebApplication1WebHook
+{
+    // Class to handle JSON webhooks
+    public class GenericJsonWebHookHandler : WebHookHandler
+    {
+        public GenericJsonWebHookHandler()
+        {
+            this.Receiver = "genericjson";
+        }
+
+        // Method to run when a webhook is received
+        public override async Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            JObject data = context.GetDataOrDefault<JObject>();
+
+            try
+            {
+                // Get the webhook topic from the request headers
+                string topic = context.Request.Headers.GetValues("X-WC-Webhook-Topic").FirstOrDefault();
+
+                HomeController controller = new HomeController();
+
+                // Handle customer created
+                if (topic != null && topic.ToLower().Equals("customer.created"))
+                {
+                    dynamic dData = data;
+                    string username = dData.username;
+                    string email = dData.email;
+                    string firstName = dData.first_name;
+                    string lastName = dData.last_name;
+
+                    await controller.CreateCustomer(username, email, firstName, lastName);
+                }
+                // Handle order created
+                else if (topic != null && topic.ToLower().Equals("order.created"))
+                {
+                    dynamic dData = data;
+                    string customerEmail = dData.billing.email;
+                    string productNumber = dData.line_items[0].product_id.ToString();
+                    decimal quantity = dData.line_items[0].quantity;
+
+                    await controller.CreateSalesOrder(customerEmail, productNumber, quantity);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Debug
+                System.Diagnostics.Debug.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
